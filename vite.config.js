51a9ed4e80358a804e0e7e4d4c0717f43d9efa06
gvnrds Windows Desktop <gvnrx@users.noreply.github.com>
2025-08-16 09:52:47 -0400

    updated predeploy in package.json and updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/React-O-Timer/",
+  base: mode === "production" ? "/React-O-Timer/" : "/",
   test: {
     environment: "jsdom", // ✅ Simulates browser
     globals: true, // ✅ (optional) lets you use `describe`, `it` without importing
     setupFiles: "./test/setup.js",
   },
-});
+}));
